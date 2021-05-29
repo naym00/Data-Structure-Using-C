0cea6e9968af101b85f94b43d2ef99dc9adb2a2e
naym00 <68757008+naym00@users.noreply.github.com>
2021-05-29 14:13:48 +0600

    Add files via upload

---
+++ Void Pointer LinkList/LinkList using Void pointer.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+typedef struct _node
+{
+    void *data;
+    size_t memorySize;
+    struct _node *next;
+} node;
+node *head=NULL;
+node *last=NULL;
+void createNodeAndInsetData(void *data, size_t memorySize)
+{
+    int i;
+    node* newNode = (node*)malloc(sizeof(node));
+    newNode->data=malloc(memorySize);
+    for(i=0; i<memorySize; i++)
+    {
+        *((char *)newNode->data+i)=*((char *)data+i);
+    }
+    newNode->memorySize=memorySize;
+    newNode->next=NULL;
+    if(head==NULL)
+    {
+        head=newNode;
+        last=newNode;
+    }
+    else
+    {
+        last->next=newNode;
+        last=newNode;
+    }
+}
+node * print(node *prnt)
+{
+    if(prnt!=NULL)
+    {
+        if(prnt->memorySize==4)
+        {
+            printf("%d ", *(int *)prnt->data);
+        }
+        else if(prnt->memorySize==8)
+        {
+            printf("%0.2lf ", *(double *)prnt->data);
+        }
+        return print(prnt->next);
+    }
+    printf("\n");
+    return;
+}
+int main()
+{
+    int intData;
+    double doubleData;
+
+    intData=10;
+    createNodeAndInsetData(&intData, sizeof(int));
+    intData=20;
+    createNodeAndInsetData(&intData, sizeof(int));
+
+    doubleData=10.5;
+    createNodeAndInsetData(&doubleData, sizeof(double));
+    doubleData=30.56;
+    createNodeAndInsetData(&doubleData, sizeof(double));
+
+    print(head);
+    return 0;
+}
